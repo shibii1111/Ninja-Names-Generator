6944560c90fb950f794bea039033260f8d8d0472
shibii <shiibii1111@gmail.com>
2021-05-26 14:57:09 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const data = require('./NinjaNames.json');
+const cors = require('cors');
+const e = require('express');
+
+const app = express();
+
+app.set('json spaces', 1);
+app.use(cors())
+
+app.use('/ninjify', (req, res, next) => {
+    const filters = req.query;
+    const filteredNinjas = data.filter(ninja =>{
+        let isValid = true;
+        for (key in filters){
+            isValid = isValid && ninja[key] == filters[key];
+        }
+        return isValid;
+    });
+    if(filteredNinjas){
+        res.send(filteredNinjas);
+    }
+});
+
+app.listen(3001, () => console.log("app runs"));
