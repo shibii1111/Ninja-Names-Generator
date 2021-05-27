3ee9370232a9a959a51e78661d94cbbd21da420f
shibii <shiibii1111@gmail.com>
2021-05-26 23:54:40 -0400

    Fixing erros

--- app.js
+++ app.js
@@ -22,3 +22,7 @@ app.use('/ninjify', (req, res, next) => {
         res.send(filteredNinjas);
     }
 });
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
